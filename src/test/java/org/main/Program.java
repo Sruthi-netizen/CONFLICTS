c1889926c16374162c90fc25806662af4ecd932e
Sruthi-netizen <sru.2312@gmail.com>
2021-02-17 16:26:47 +0530

    two branch

--- src/test/java/org/main/Program.java
+++ src/test/java/org/main/Program.java
@@ -2,8 +2,8 @@ package org.main;
 
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hi");
 		System.out.println("YOU");
+		System.out.println("Hi");
 		System.out.println("Hey");
 	}
 }
